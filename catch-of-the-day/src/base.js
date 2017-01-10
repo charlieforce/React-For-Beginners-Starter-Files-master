f5190eabd766f6f852cc9c1ed02c7b0e17abed38
CharlieKB <charlieforce1999@gmail.com>
2017-01-09 16:38:57 -0800

    First commit
    A simple react app for a fish market

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBNtOeFHo7VkADTh8Pn3pOmGYvMBVX1EFM",
+  authDomain: "catch-of-the-day-charlie-kb.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-charlie-kb.firebaseio.com",
+});
+
+export default base;
